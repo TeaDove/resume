0808b186d4a08125be624e73b17f309b82b2d625
pibragimov <pibragimov@whoosh.bike>
2023-02-08 17:57:11 +0300

    feat: add receipt template

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+### See more in https://github.com/gech4me/recipe-app
---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = '53d6181b';
+  const APP_KEY = 'bec2effb00a6d049a171d5cc2faca269';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+  return (
+    <div className="App">
+      <form action="#" className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
